31e84302312460094978fa4ff73ae983c751be93
Peter Jones <pjones@pmade.com>
2015-07-31 13:00:31 -0600

    This script has been replaced with another

--- app/x11vnc.zsh
+++
@@ -1,9 +0,0 @@
-################################################################################
-function x11vnc-projector () {
-  projector=`xrandr|egrep '^HDMI1[[:space:]]+connected'|cut -f3 -d' '`
-
-  x11vnc -clip "$projector" -viewonly \
-         -forever -localhost -quiet \
-         -wirecopyrect -scrollcopyrect \
-         -scale 0.9
-}
