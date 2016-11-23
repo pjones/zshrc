aa19f45e04bc1148779a03c9fa2ef820b7c90cad
Peter Jones <pjones@devalot.com>
2016-11-23 11:27:48 -0700

    Fix gpg-agent environment

--- lib/agents.zsh
+++ lib/agents.zsh
@@ -1,9 +1,7 @@
-# Some variables to always export.
-export SSH_AGENT_PID
-export SSH_AUTH_SOCK
-export GPG_AGENT_INFO
-
 # Make sure everyone knows where gpg files are:
 export GNUPGHOME=$HOME/keys/gpg
 export GPG_TTY=`tty`
 export SSH_AUTH_SOCK="$HOME/.gnupg/S.gpg-agent.ssh"
+
+# Update gpg-agent environment.
+gpg-connect-agent updatestartuptty /bye >/dev/null
