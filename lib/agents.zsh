5d13412bbdd3bf332c4b39874e73eb2b2af34376
Peter Jones <pjones@devalot.com>
2017-03-28 12:33:53 -0700

    Added services for mpd, gpg-agent, and autosshfs.
    Also, cleaned up the screenlock service.

--- lib/agents.zsh
+++
@@ -1,7 +0,0 @@
-# Make sure everyone knows where gpg files are:
-export GNUPGHOME=$HOME/keys/gpg
-export GPG_TTY=`tty`
-export SSH_AUTH_SOCK="$HOME/.gnupg/S.gpg-agent.ssh"
-
-# Update gpg-agent environment.
-gpg-connect-agent updatestartuptty /bye >/dev/null
