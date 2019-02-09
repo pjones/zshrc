cbe02036f9ac7af2b8a7e310dd5b5cfbb093dde3
Peter Jones <pjones@devalot.com>
2019-02-08 20:03:12 -0700

    bindings: vi bindings with some Emacs flare

--- lib/bindings.zsh
+++ lib/bindings.zsh
@@ -9,12 +9,30 @@ match-words-by-style normal
 
 # my widgets
 for widget in last-cmd-as-expansion; do
-    autoload $widget
-    zle -N $widget
+  autoload $widget
+  zle -N $widget
 done
 
-# default key binding is Emacs:
-bindkey -e
+# default key binding is vi:
+bindkey -v
+
+# Indicate which vi mode you're in:
+function zle-keymap-select {
+  case $KEYMAP in
+    vicmd)
+      vi_mode="%K{yellow}%F{black} <N> %f%k"
+      ;;
+
+    *)
+      vi_mode=""
+      ;;
+  esac
+
+  RPROMPT="$vi_mode"
+  zle reset-prompt
+}
+
+zle -N zle-keymap-select
 
 # Edit command lines in EDITOR:
 autoload edit-command-line
@@ -25,12 +43,16 @@ autoload copy-earlier-word
 zle -N copy-earlier-word
 
 # Insert some custom keybindings
-bindkey -e '^x^e' edit-command-line
-bindkey -e '^x^p' push-line-or-edit
-bindkey -e '^x^h' run-help
-bindkey -e '^x^i' last-cmd-as-expansion
-bindkey -e '^r'   history-incremental-search-backward
-bindkey -e '^w'   backward-kill-word-match
-bindkey -e '^i'   expand-or-complete-prefix
-bindkey -e '^[_'  insert-last-word
-bindkey -e '^[-'  copy-earlier-word
+bindkey '^b'   backward-char
+bindkey '^f'   forward-char
+bindkey '^i'   expand-or-complete-prefix
+bindkey '^n'   down-history
+bindkey '^p'   up-history
+bindkey '^r'   history-incremental-search-backward
+bindkey '^w'   backward-kill-word-match
+bindkey '^x-'  copy-earlier-word
+bindkey '^x^e' edit-command-line
+bindkey '^x^h' run-help
+bindkey '^x^i' last-cmd-as-expansion
+bindkey '^x^p' push-line-or-edit
+bindkey '^x_'  insert-last-word
