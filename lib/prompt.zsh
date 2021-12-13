b1383ceb47976ecdbffd6dfcc548a649db5491a4
Peter Jones <pjones@devalot.com>
2021-12-13 08:59:38 -0700

    vterm: Re-introduce vterm tracking
    I'm using vterm again, seems to be more stable than in the past.

--- lib/prompt.zsh
+++ lib/prompt.zsh
@@ -16,6 +16,28 @@ if [ -n "$ZSH_SHOW_VCS" ]; then
   zstyle ':vcs_info:*' check-for-changes true
 fi
 
+# https://github.com/akermu/emacs-libvterm
+vterm_printf() {
+  if [ -n "$TMUX" ]; then
+    # Tell tmux to pass the escape sequences through
+    # (Source: http://permalink.gmane.org/gmane.comp.terminal-emulators.tmux.user/1324)
+    printf "\ePtmux;\e\e]%s\007\e\\" "$1"
+  elif [ "${TERM%%-*}" = "screen" ]; then
+    # GNU screen (screen, screen-256color, screen-256color-bce)
+    printf "\eP\e]%s\007\e\\" "$1"
+  else
+    printf "\e]%s\e\\" "$1"
+  fi
+}
+vterm_prompt_end() {
+  vterm_printf "51;A$(whoami)@$(hostname):$(pwd)"
+}
+vterm_title() {
+  print -Pn "\e]2;%m:%2~\a"
+}
+add-zsh-hook -Uz chpwd vterm_title
+vterm_title # Call now to set initial title.
+
 function precmd() {
   PS1='$ '
   RPROMPT=''
@@ -48,6 +70,7 @@ function precmd() {
     fi
 
     PS1="${top_prompt} ${info_prompt}%F{red}]%f"$'\n'"${last_exit}"'%(!.#.$) '
+    PS1="${PS1}%{$(vterm_prompt_end)%}" # Add vterm prompt tracking.
     ;;
   esac
 }
